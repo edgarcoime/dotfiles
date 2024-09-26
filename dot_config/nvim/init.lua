be6b6bacf49dd9bad99ca0b56ba121e71e83e79f
Edgar Coime <e.coimeii@gmail.com>
2024-09-26 13:35:47 -0700

    feat: add alacritty folder for version control

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,14 +1,14 @@
-require 'core'
+require("core")
 --
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then
-    error('Error cloning lazy.nvim:\n' .. out)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,16 +23,16 @@ vim.opt.rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup({
-  { import = 'plugins' },
-  { import = 'plugins.lsp' },
+require("lazy").setup({
+	{ import = "plugins" },
+	{ import = "plugins.lsp" },
 }, {
-  install = {},
-  checker = {
-    enabled = true,
-    notify = true,
-  },
-  change_detection = {
-    notify = false,
-  },
+	install = {},
+	checker = {
+		enabled = true,
+		notify = true,
+	},
+	change_detection = {
+		notify = false,
+	},
 })
