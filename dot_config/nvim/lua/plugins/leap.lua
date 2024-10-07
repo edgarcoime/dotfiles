c67e860598348cc55eaeaf0d1537575a792b1d4f
Edgar Coime <e.coimeii@hotmail.com>
2024-10-06 21:25:16 -0700

    file: delete nvim version controm

--- dot_config/nvim/lua/plugins/leap.lua
+++
@@ -1,8 +0,0 @@
--- Leap motions to easily navigate document
-return {
-  -- 'ggandor/leap.nvim',
-  -- config = function(plugin, opts)
-  --   require('leap').add_default_mappings()
-  -- end,
-  -- event = 'BufEnter',
-}
