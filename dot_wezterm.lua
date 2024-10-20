f2ffd1914eb068c450e5e9f85963a4e14feb7221
Edgar Coime <e.coimeii@hotmail.com>
2024-10-19 19:01:42 -0700

    feat: add wezterm

---
+++ dot_wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+
+-- contains configuration
+local config = wezterm.config_builder()
+
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 18
