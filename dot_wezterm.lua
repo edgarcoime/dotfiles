5617d070c89172cbdfc59c34181b99151e29107b
Edgar Coime <e.coimeii@hotmail.com>
2024-10-19 22:32:15 -0700

    feat: add monaspace

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -3,7 +3,20 @@ local wezterm = require("wezterm")
 -- contains configuration
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
+-- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+-- https://github.com/githubnext/monaspace#monaspace
+-- https://monaspace.githubnext.com/
+config.font = wezterm.font_with_fallback({
+  {
+    family = "Monaspace Neon",
+    weight = "Medium",
+    harfbuzz_features = { "calt", "ss01", "ss02", "ss03", "ss05", "ss07", "ss08", "ss09" },
+  },
+  { family = "Symbols Nerd Font", weight = "Medium" },
+  -- { family = "JetBrainsMono NFM", weight = "Medium" },
+})
+
 config.font_size = 18
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
