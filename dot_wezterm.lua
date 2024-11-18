96645ee2fdae69b647be0fe85712f8dc016ac67b
Edgar Coime <e.coimeii@hotmail.com>
2024-11-18 14:10:24 -0800

    fix: wezterm refresh rate and smoother animations

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -7,11 +7,15 @@ local config = wezterm.config_builder()
 -- https://github.com/linkarzu/dotfiles-latest/blob/main/wezterm/wezterm.lua
 config.term = "xterm-kitty"
 config.enable_kitty_graphics = true
-config.max_fps = 120
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
+
+-- https://wezfurlong.org/wezterm/config/lua/config/animation_fps.html?h=animation
+config.animation_fps = 60
+-- https://wezfurlong.org/wezterm/config/lua/config/max_fps.html
+config.max_fps = 120
 
 -- Fixes small icons
 config.allow_square_glyphs_to_overflow_width = "Always"
@@ -23,9 +27,10 @@ config.color_scheme = "tokyonight_night"
 config.window_background_opacity = 0.92
 config.macos_window_background_blur = 50
 
+-- config.default_cursor_style = "SteadyBlock"
 config.default_cursor_style = "BlinkingBlock"
-config.cursor_blink_ease_out = "Constant"
-config.cursor_blink_ease_in = "Constant"
+-- config.cursor_blink_ease_out = "Constant"
+-- config.cursor_blink_ease_in = "Constant"
 
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 -- config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
